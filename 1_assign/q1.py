e18ec1800a81d16dc3efa343ad3a622b97bde257
Rohan Awhad <rohanavad007@gmail.com>
2023-09-21 17:19:40 -0700

    added testcases and solved  minor bug

--- 1_assign/q1.py
+++ 1_assign/q1.py
@@ -2,45 +2,43 @@
 import sys
 import os
 from loguru import logger
-logger.remove()
-logger.add('logs')
-with open('logs', 'a') as f: f.write('\n'+'-'*50+'\n')
+def log(t):
+  with open('logs', 'a') as f: f.write('\n'+t)
+  
+log('-'*50)
 
 DELIM = '$'
 OUTPUTS_DIR = 'outputs'
 os.makedirs(OUTPUTS_DIR, exist_ok=True)
 
-def z_algorithm(text: str, ptrn: str):
+def z_algorithm(s: str):
   left = right = 0
-  s = ptrn + DELIM + text
   z_array = [0] * len(s)
-  logger.debug(s[len(ptrn)+1:])
 
   for i in range(1, len(s)):
-    logger.debug(f'z array: {list(zip(s, z_array))}')
+    log(f'z array: {list(zip(s, z_array))}')
     if i > right:
       #create a new z-box
       left = right = i
       while (right < len(s)) and (s[right] == s[right-left]): right += 1
       right -= 1
       z_array[i] = right - left + 1
-      logger.debug(s[left:right+1])
+      log(s[left:right+1])
     else:
       # you are in a z-box
-      j = z_array[i-left]
+      j = i-left
       val = z_array[j]
-      logger.debug(val+i)
+      log(f'j: {j} | val: {val} | left: {left} | right: {right} | i: {i}')
       # check if the value + i is greater than right
       if (val+i) > right:  # TODO (rohan): confirm '>' or '>='
         # shift left to i, and start a search for new right of the z-box
-        left = j
+        left = i
         while (right < len(s)) and (s[right] == s[right-left]): right += 1
         right -= 1
         z_array[i] = right - left + 1
-      else:
-        z_array[i] = val
+      else: z_array[i] = val
 
-  logger.debug(f'final z array: {z_array}')
+  log(f'final z array: {z_array}')
   return z_array
 
 
@@ -49,16 +47,17 @@ if __name__ == '__main__':
   else: raise ValueError('expected filename as an argument. didn\'t recieve anything')
 
   with open(fn, 'r') as f: text, ptrn, *_ = f.read().split('\n')
-  logger.debug(f'text: {text}')
-  logger.debug(f'ptrn: {ptrn}')
+  log(f'text: {text}')
+  log(f'ptrn: {ptrn}')
 
-  z_array = z_algorithm(text, ptrn)
+  s = ptrn + DELIM + text
+  z_array = z_algorithm(s)
   ans = []
   for i, x in enumerate(z_array):
     if x == len(ptrn):
       # zero-based indexing
       j = i-len(ptrn)-1
-      logger.debug(text[j:i-1])
+      log(text[j:i-1])
       ans.append(j+1)  # converting to one-based indexing
 
-  with open(f'{OUTPUTS_DIR}/{fn.split("/")[-1]}', 'w') as f: f.write('\n'.join(map(str, ans)))
+  with open(f'{OUTPUTS_DIR}/sol_{fn.split("_")[-1]}', 'w') as f: f.write('\n'.join(map(str, ans)) + '\n')
---
+++ 1_assign/test_z_array.py
@@ -0,0 +1,42 @@
+#!/opt/homebrew/bin/python3
+
+from q1 import z_algorithm
+
+import os
+
+DELIM = '$'
+
+def test_1():
+  s = 'rohanrorohrohanrorohan'
+  exp = [0, 0, 0, 0, 0, 2, 0, 3, 0, 0, 10, 0, 0, 0, 0, 2, 0, 5, 0, 0, 0, 0]
+  act = z_algorithm(s)
+  assert exp == act, f'exp: {exp}\nact:{act}'
+
+def test_2():
+  ex_dir = 'examples'
+  qa_pairs = [
+    ('ex_0', 'exsol_0'),
+    ('ex_1', 'exsol_1'),
+    ('ex_2', 'exsol_2'),
+  ]
+
+  for q, a in qa_pairs:
+    fn = f'{ex_dir}/{q}'
+    with open(f'{ex_dir}/{q}', 'r') as f: text, ptrn, *_ = f.read().split('\n')
+    with open(f'{ex_dir}/{a}', 'r') as f: exp = [int(x) for x in f.read().split('\n') if x]
+
+    s = ptrn + DELIM + text
+    z_array = z_algorithm(s)
+    act = []
+    for i, x in enumerate(z_array):
+      if x == len(ptrn):
+        # zero-based indexing
+        j = i-len(ptrn)-1
+        act.append(j+1)  # converting to one-based indexing
+
+    assert exp == act, f'exp: {exp}\nact:{act}'
+
+
+if __name__ == '__main__':
+  #test_1()
+  test_2()
