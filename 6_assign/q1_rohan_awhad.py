20d63a5b5a29401c0eb27d5455f3e146102be088
Rohan Awhad <rohanavad007@gmail.com>
2023-12-07 11:38:52 -0700

    Implement hard k-means clustering algorithm

--- 6_assign/q1_rohan_awhad.py
+++ 6_assign/q1_rohan_awhad.py
@@ -11,12 +11,25 @@ each representing a coordinate value for a dimension of a data point in Euclidea
 > Output: k centers obtained from running Lloyd algorithm – k lines (one for each center), each con-
 taining m coordinate values for the center
 """
+import random
+import math
 
-import numpy as np
-
-def lloyd_algorithm(k, m, data_points):
+def distance(point1, point2):
   """
-  Implements the Lloyd algorithm for k-means clustering.
+  Calculates the Euclidean distance between two points.
+
+  Args:
+  point1 (list of float): The first point.
+  point2 (list of float): The second point.
+
+  Returns:
+  float: The Euclidean distance between the two points.
+  """
+  return math.sqrt(sum((p1 - p2) ** 2 for p1, p2 in zip(point1, point2)))
+
+def lloyd_algorithm(k, data_points):
+  """
+  Implements the Lloyd algorithm for hard k-means clustering.
 
   Args:
   k (int): The number of clusters.
@@ -27,28 +40,33 @@ def lloyd_algorithm(k, m, data_points):
   list of list of float: The final centers of the clusters.
   """
   # Initialize centers randomly
-  centers = data_points[np.random.choice(len(data_points), k, replace=False)]
+  centers = random.sample(data_points, k)
 
   while True:
     # Assign each point to the closest center
     clusters = [[] for _ in range(k)]
     for point in data_points:
-      distances = [np.linalg.norm(np.array(point) - np.array(center)) for center in centers]
-      closest_center = np.argmin(distances)
+      distances = [distance(point, center) for center in centers]
+      closest_center = min(range(k), key=lambda i: distances[i])
       clusters[closest_center].append(point)
 
     # Update the centers to be the mean of the points in each cluster
     new_centers = []
     for cluster in clusters:
-      new_centers.append(np.mean(cluster, axis=0) if cluster else np.random.choice(data_points, 1)[0])
+      if cluster:
+        new_center = [sum(coords) / len(coords) for coords in zip(*cluster)]
+      else:
+        new_center = random.choice(data_points)
+      new_centers.append(new_center)
 
     # Check for convergence (if centers do not change)
-    if np.array_equal(centers, new_centers):
+    if centers == new_centers:
       break
     else:
       centers = new_centers
 
   return centers
+
 
 # Example usage
 k, m = 3, 2  # 3 clusters, 2 dimensions
@@ -57,5 +75,9 @@ data_points = [[1, 2], [1, 4], [1, 0],
          [20, 2], [20, 4], [20, 0]]  # Example data points
 
 # Run Lloyd's algorithm
-centers = lloyd_algorithm(k, m, data_points)
-print(centers)
+centers = lloyd_algorithm(k, data_points)
+
+# pretty print
+for centroid in centers:
+  for i in range(m): print(centroid[i], end=" ")
+  print()
